464bfb79ad4ac9b2c067e2b6d37a01c955d21a4f
colin sadowitz <sadowitzc@gmail.com>
2025-01-21 14:28:24 -0500

    Set Starting Hands

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from src.Game import Game
 
 def main():
 	game = Game()
+	game.shuffle_deck()
+	names = ["player_1", "player_2", "player_3", "player_4"]
+	game.set_starting_hands(names)
+	print (game.player_hands)
 
 if __name__ == "__main__":
 	main()
--- src/Game.py
+++ src/Game.py
@@ -1,17 +1,45 @@
 from src.Deck import Deck
+import random
 
 class Game:
-	def __init__(self):
-		# List of cards = Deck
-		self.deck = Deck().get_deck()
-		# List of cards = discard
-		self.discard = []
-		# Dict of players and a list of lists for each sorted card in their hand
+	def __init__(self, deck=None, discard=None, starting_hand=7):
+		if (deck is None):
+			deck = Deck().get_deck()
+		self.deck = deck
+
+		if (discard is None):
+			discard = []
+		self.discard = discard
+
+		self.starting_hand = starting_hand
+
+		# player: [[r], [g], [b], [y], [w]]
 		self.player_hands = {}
 
-	def shuffle_deck():
-		pass
+	def shuffle_deck(self):
+		size = len(self.deck)
+		for i in range(size):
+			random_index = random.randint(0, size - 1)
+			self.deck[i], self.deck[random_index] = self.deck[random_index], self.deck[i]
 
-	def set_starting_hands(player_names):
-		# for (player in player_names):
-		pass
+	def set_starting_hands(self, player_names):
+		for player in player_names:
+			red = []
+			green = []
+			blue = []
+			yellow = []
+			wild = []
+			for i in range(self.starting_hand):
+				card = self.deck[i]
+				if card[0] == 'R':
+					red.append(card)
+				elif card[0] == 'G':
+					green.append(card)
+				elif card[0] == 'B':
+					blue.append(card)
+				elif card[0] == 'Y':
+					yellow.append(card)
+				elif card[0] == 'W':
+					wild.append(card)
+				self.deck.remove(card)
+			self.player_hands[player] = [red, green, blue, yellow, wild]
